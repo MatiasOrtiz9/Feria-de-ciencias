144324c876ff61b7f59bfba3c33f89514ba7d237
MatiasOrtiz9 <Matiasdarioortiz@gmail.com>
2023-11-05 23:11:12 -0300

    asas

--- proyecto/informacion/encuestas/contenido.js
+++ proyecto/informacion/encuestas/contenido.js
@@ -88,7 +88,7 @@ function loadScore() {
     const button = document.createElement("button");
     button.innerText = "Volver";
     button.addEventListener("click", function () {
-        window.location.href = "/proyecto/informacion";
+        window.location.href = "/proyecto/informacion/index.html";
     });
 
     const buttonContainer = document.createElement("div");
